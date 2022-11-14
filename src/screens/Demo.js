a014d877895be3857ecb3216029325613cdc5b4f
Somnath Prasad <somnathprasad559@gmail.com>
2022-11-14 14:39:50 +0530

    initial

---
+++ src/screens/Demo.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export function Demo() {
+  return (
+    <View className="h-full flex-1 w-full items-center justify-center">
+      <Text className="text-center">
+        This is the demo screen
+      </Text>
+    </View>
+  );
+}

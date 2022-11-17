74ab09f0fac69d53a133aa838caab30ab7bc85ea
iamausafhussain <ausafhussain010@gmail.com>
2022-11-17 16:32:02 +0530

    added image carousel

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from '@react-navigation/native';
-import { Demo } from "./src/screens"
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Slider from "./src/screens/Slider";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Demo" component={Demo} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<SafeAreaProvider>
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen name="Slider" component={Slider} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</SafeAreaProvider>
+	);
 }
